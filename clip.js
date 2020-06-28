298e483b939c54fdd5d2e67aef966f2ef951835e
jopemachine <jopemachine@naver.com>
2020-06-27 22:12:00 -0700

    Add cliped_note_tag option

--- clip.js
+++ clip.js
@@ -1,19 +1,26 @@
 const clipboardy = require('clipboardy');
+const config = require('./config.json');
 
 require('@jxa/global-type');
 const run = require('@jxa/run').run;
 
-const createNoteByText = (clipStr) => {
+const createNoteByText = (clipStr, tag) => {
 
-  let callback = (clipStr) => {
+  let callback = (clipStr, tag) => {
     // below is JXA
     const Evernote = Application("Evernote");
     Evernote.createNote({ withText: clipStr });
+
+    if(tag !== "") {
+      Evernote.createNote({ withText: clipStr, tags: [tag] });
+    } else {
+      Evernote.createNote({ withText: clipStr });
+    }
   }
-  
-  return run(callback, clipStr);
+
+  return run(callback, clipStr, tag);
 }
 
 clipboardy.read().then(async data => {
-  await createNoteByText(data);
+  await createNoteByText(data, config.cliped_note_tag);
 });
--- clipAndOpen.js
+++ clipAndOpen.js
@@ -1,21 +1,30 @@
 const clipboardy = require('clipboardy');
+const config = require('./config.json');
 
 require('@jxa/global-type');
 const run = require('@jxa/run').run;
 
-const createNoteByTextAndOpen = (clipStr) => {
+const createNoteByTextAndOpen = (clipStr, tag) => {
 
-  let callback = (clipStr) => {
+  let callback = (clipStr, tag) => {
     // below is JXA
     const Evernote = Application("Evernote");
     Evernote.activate();
-    const createdNote = Evernote.createNote({ withText: clipStr });
+
+    let createdNote;
+
+    if(tag !== "") {
+      createdNote = Evernote.createNote({ withText: clipStr, tags: [tag] });
+    } else {
+      createdNote = Evernote.createNote({ withText: clipStr });
+    }
+
     Evernote.openNoteWindow({ with: createdNote });
   }
 
-  return run(callback, clipStr);
+  return run(callback, clipStr, tag);
 }
 
 clipboardy.read().then(async data => {
-  await createNoteByTextAndOpen(data);
+  await createNoteByTextAndOpen(data, config.cliped_note_tag);
 });
