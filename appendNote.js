069d38fcd8cfd7a684c530d07dece832f52a2772
jopemachine <jopemachine@naver.com>
2020-08-30 09:01:42 +0900

    Update to 0.7.9

---
+++ appendNote.js
@@ -0,0 +1,28 @@
+const config = require('./searchConfig.json');
+
+require('@jxa/global-type');
+const run = require('@jxa/run').run;
+
+const rawInput = process.argv.slice(2).join(" ").normalize();
+
+const [notelink, ...inputs] = rawInput.split(" ");
+const input = "\n" + inputs.join(" ");
+
+const appendNote = (notelink, input, createAndOpen) => {
+
+  const callback = (notelink, input, createAndOpen) => {
+    const Evernote = Application("Evernote");
+
+    const targetNote = Evernote.findNote(notelink)
+
+    Evernote.append(targetNote, { text: input });
+
+    if(createAndOpen === true) {
+      Evernote.openNoteWindow({ with: targetNote });
+    }
+  }
+
+  return run(callback, notelink, input, createAndOpen);
+}
+
+appendNote(notelink, input, config.create_and_open);
