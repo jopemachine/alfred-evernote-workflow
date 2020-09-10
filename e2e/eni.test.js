75ff663a20c04e341a449e680e6488af79f0268b
jopemachine <jopemachine@naver.com>
2020-09-10 19:02:27 +0900

    Add husky, ava setting, eni test

---
+++ e2e/eni.test.js
@@ -0,0 +1,26 @@
+import test from 'ava';
+import alfyTest from 'alfy-test';
+
+test('"eni" result should contains input keyword', async (t) => {
+  const testTarget = alfyTest();
+
+  const query = "alfred";
+
+  const result = await testTarget(`searchNote.js '${query}' --intitle`);
+
+  let first = true;
+
+  console.log("Query count: " + result.length);
+
+  for (let item of result) {
+    // first record shows log meaning No notes found or log showing note count
+    if (first) {
+      first = false;
+      continue;
+    }
+
+    t.assert(item.title.toLowerCase().includes(query.toLowerCase()) === true);
+  }
+
+  t.pass();
+});
